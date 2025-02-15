b658d0d01d82b4962da10d9a78bffa3368f05fa5
Sasankredd <126907638+Sasankredd@users.noreply.github.com>
2025-02-15 15:26:53 +0530

    EXCEPTION-HANDLING-LOCALLY

--- src/main/java/com/example/rest/RESTCONTROLLER.java
+++ src/main/java/com/example/rest/RESTCONTROLLER.java
@@ -1,10 +1,11 @@
 package com.example.rest;
 
 import com.example.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.StudentRespaonse;
+import com.example.exception.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +25,19 @@ public class RESTCONTROLLER {
 
     @GetMapping("/students/{studentid}")
     public Student getsingle(@PathVariable int studentid){
+        if((studentid >= students.size()) || (studentid < 0)){
+            throw new StudentNotFoundException("student id not found " + studentid);
+        }
         return students.get(studentid);// // JACKSON - CONVERT TO JSON OBJECT
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentRespaonse> handleException(StudentNotFoundException e){
+        StudentRespaonse error = new StudentRespaonse(); // object
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<StudentRespaonse>(error, HttpStatus.NOT_FOUND);
+
     }
 }
